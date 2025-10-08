08f204db31e6aed14109684c1bc1294c059511e0
unaleotromundo <unaleotromundo@gmail.com>
2025-10-08 20:34:38 -0300

    ff

--- main.py
+++ main.py
@@ -152,7 +152,6 @@ if st.session_state.page == 'home':
     # Título y subtítulo
     st.markdown('<div class="titulo-minimal">Mirá Cho!</div>', unsafe_allow_html=True)
     st.markdown('<div class="subtitulo-minimal">Directorio de Temas | Estilo Consola Minimalista</div>', unsafe_allow_html=True)
-    st.subheader("Selecciona con dos clic un Tema para Cargar la Vista Completa")
     st.markdown("---")
 
     # Usamos 3 columnas, pero las tarjetas son más pequeñas.
